24e81e7bcaaa85553b106cc71c13849994ccbf18
ana <gradanamaria359@gmail.com>
2024-04-30 12:41:31 +0200

    PRIMER COMMIT DESPUES DE LA MODIFICACION

--- src/gitPrueba/GitPrueba.java
+++ src/gitPrueba/GitPrueba.java
@@ -4,6 +4,7 @@ public class GitPrueba {
 
 	public static void main(String[] args) {
 		System.out.println("Esto es una prueba de GitHub en Eclipse");
+		System.out.println("Esto es una modificación");
 	}
 
 }
