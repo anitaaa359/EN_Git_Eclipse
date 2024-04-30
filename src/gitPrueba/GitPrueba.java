e9e6a62ee96110a871d8ad11b5da41a43a21c41b
ana <gradanamaria359@gmail.com>
2024-04-30 12:43:22 +0200

    SEGUNDO COMMIT DESPUES DE HACER OTRA MODIFICACION

--- src/gitPrueba/GitPrueba.java
+++ src/gitPrueba/GitPrueba.java
@@ -5,6 +5,7 @@ public class GitPrueba {
 	public static void main(String[] args) {
 		System.out.println("Esto es una prueba de GitHub en Eclipse");
 		System.out.println("Esto es una modificación");
+		System.out.println("Esto es otra modificacion");
 	}
 
 }
